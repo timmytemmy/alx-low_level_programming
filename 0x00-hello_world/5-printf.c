970583e0bd06d742cf4fa0517ec38843dceb35b0
timmytemmy <timmytemmy241@gmail.com>
2022-10-13 19:20:30 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<studio.h>
+#include <stdio.h>
 /**
- * Descricption: main -Prints out a string to stdout.
+ * Description: main - prints out a string to stdout
  * Return: Always zero
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n)
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
