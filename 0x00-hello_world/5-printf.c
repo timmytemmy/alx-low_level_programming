88a31128216d0533f5a79d3152a24391fe25bcd2
timmytemmy <timmytemmy241@gmail.com>
2022-10-13 23:23:55 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ * Description: using the main function
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
