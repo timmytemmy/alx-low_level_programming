951a74fb3545ba65841fa41118eb3184628958d5
timmytemmy <timmytemmy241@gmail.com>
2022-10-13 18:14:55 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * Descricption: main -Prints out a string to stdout.
+ * Return: Always zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n)
+		return (0);
+}
