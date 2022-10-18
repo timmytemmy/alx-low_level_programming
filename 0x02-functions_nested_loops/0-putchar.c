09e5d63756c511deddbbef073908546dc0e505fa
timmytemmy <timmytemmy241@gmail.com>
2022-10-18 04:11:28 -0700

    Function 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Prints _putchar
@@ -7,6 +7,7 @@
  */
 
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDE
+#define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
 void print_alphabet(void);
@@ -13,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print times_table(int n);
+void print_times_table(int n);
 
 #endif
