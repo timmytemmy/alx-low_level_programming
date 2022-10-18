b5c00202b27b09d1956ae60e015dc616c8734693
timmytemmy <timmytemmy241@gmail.com>
2022-10-18 02:38:48 -0700

    Function 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - Prints _putchar
+ * Description: Using the main function
+ * Return: Always zero (0)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is an ALX task on C Functions and nested loops
