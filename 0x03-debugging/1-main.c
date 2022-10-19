eb6cb7d634f5f0e409679166863d3e6cc47b67c0
timmytemmy <timmytemmy241@gmail.com>
2022-10-19 05:28:00 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int i;
+printf("infinite loop incoming :(\n");
+
+i = 0
+/*
+ * while (i < 0)
+ * {
+ * 		putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\0/\n");
+
+return (0);
+}
