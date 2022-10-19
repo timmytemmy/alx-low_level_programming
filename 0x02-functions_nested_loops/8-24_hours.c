155a0660d25fbb848f9922c98b291ff756c9b3db
timmytemmy <timmytemmy241@gmail.com>
2022-10-18 21:08:24 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,25 +8,18 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int minutes_remainder;
+	int h, m;
 
-	while (hours <= 23)
+	for (h = 0; h <= 23; h++)
 	{
-		while (minutes <= 59)
+		for (m = 0; m <= 59; m++)
 		{
-			hours_remainder = hours % 10;
-			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(minutes / 10 + '0');
-			_putchar(minutes_remainder + '0');
-			minutes++;
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
-		hours++;
-		minutes = 0;
 	}
 }
