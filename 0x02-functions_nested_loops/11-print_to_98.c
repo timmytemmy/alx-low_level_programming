9f1aee38201b3e29a6f2aa09746d5e60a910bf2d
timmytemmy <timmytemmy241@gmail.com>
2022-10-20 05:46:54 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - main
+ * @n: integer value
+ * Description: prints all natural numbers from n t0 98
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(",");
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+				if (i > 98)
+				{
+					printf(',');
+				}
+		}
+		printf('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase characters
+ *@c: parameter
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c >= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (c);
+}
