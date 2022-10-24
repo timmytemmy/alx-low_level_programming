2a29ffaa0b08a6187c16822798d35042c80acd0f
timmytemmy <timmytemmy241@gmail.com>
2022-10-24 11:20:36 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int i, max, half;
+	char first, last;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
