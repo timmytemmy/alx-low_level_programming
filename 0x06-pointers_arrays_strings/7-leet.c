b30f2e858ee1b19cb4bbd21cf54204096ddc4a8d
timmytemmy <timmytemmy241@gmail.com>
2022-10-28 00:02:32 +0100

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @c: string
+ * Return: Always 0 .
+ */
+
+char *leet(char *c)
+{
+	int len = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int up_letters[] = {65, 69, 79, 84, 76};
+	int value[] = {4, 3, 0, 7,1};
+
+	while (*(c + len) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(c + len) == low_letters[i] || *(c + len) == up_letters[i])
+			{
+				*(c + len) = value[i];
+				break;
+			}
+		}
+		len++;
+	}
+	return (s);
+}
