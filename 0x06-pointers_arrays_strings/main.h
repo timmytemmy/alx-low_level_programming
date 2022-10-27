30bdfbdad3a303732622353033b406cf57a75888
timmytemmy <timmytemmy241@gmail.com>
2022-10-27 05:44:57 -0700

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,8 @@
 #define  _MAIN_H_
 #include <stdio.h>
 
-int _putchar(char)
+int _putchar(char);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
