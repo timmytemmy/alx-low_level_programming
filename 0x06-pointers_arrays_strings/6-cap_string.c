c3338d9772e2a364f53018046374fbe07d13639a
timmytemmy <timmytemmy241@gmail.com>
2022-10-28 01:00:44 +0100

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,9 +18,9 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + len0 == words[i])
+			if (*(s + len) == words[i])
 			{
-				if ((*( s + (len + 1)) >= 97) && (*(s + len +  1)) <= 122))
+				if ((*(s + (len + 1)) >= 97) && (*(s + (len +  1)) <= 122))
 					*(s + (len + 1)) = *(s + (len + 1)) - 32;
 				break;
 			}
