86539dc46968334ecf155124a9448830dbdb7ecf
timmytemmy <timmytemmy241@gmail.com>
2022-10-28 01:33:38 +0100

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @c: string to encode
+ * Return: Always 0.
+ */
+char *rot13(char *c)
+{
+	int len, rotate;
+	char x[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char y[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		for (rotate = 0; rotate < 52; rotate++)
+		{
+			if (x[rotate] == c[len])
+			{
+				c[len] = y[rotate];
+				break;
+			}
+		}
+	}
+	return (c);
+}
