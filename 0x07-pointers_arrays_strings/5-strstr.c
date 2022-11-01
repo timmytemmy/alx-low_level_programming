e892da4101b735212ed322618fe616f4e1b44d2c
timmytemmy <timmytemmy241@gmail.com>
2022-11-01 19:01:23 +0100

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - function that loctes a substring
+ * @haystack: string.
+ * @needle: substring.
+ * Return: Always 0.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		a = 0;
+		if (haystack[a] == needle[a])
+		{
+			do
+			{
+				if (needle[a + 1] == '\0')
+					return (haystack);
+				a++;
+			}
+			while (haystack[a] == needle[a]);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
