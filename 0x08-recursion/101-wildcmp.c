976f2ed43c1e2385309f3e19c19440f4686a91bc
timmytemmy <timmytemmy241@gmail.com>
2022-11-03 03:54:08 +0100

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - returns 1 if string is a palindrome.
+ * @s1: first string.
+ * @s2: second string
+ * Return: Always 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
+
