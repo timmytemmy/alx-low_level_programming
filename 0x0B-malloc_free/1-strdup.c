6ba4bd18c68b18eca46389dd730b075c16c1993b
timmytemmy <timmytemmy241@gmail.com>
2022-11-08 11:20:37 +0100

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - using the strdup function.
+ * @str: string.
+ * Return: NULL if str is NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int a, b;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (a = 0; str[a] != '\0'; a++)
+		;
+
+	duplicate = (char *)malloc(sizeof(char) * (u + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+		duplicate[b] = str[b];
+
+	return (duplicate);
+}
