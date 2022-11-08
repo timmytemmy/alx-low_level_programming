99e51ac38386bc718c7204a02898e698bb507b62
timmytemmy <timmytemmy241@gmail.com>
2022-11-08 12:14:30 +0100

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	for (a = 0; s1[a] || s2[a]; a++)
-		;
+		c++;
+
 	concat = malloc(sizeof(char) * c);
 
 	if (concat == NULL)
