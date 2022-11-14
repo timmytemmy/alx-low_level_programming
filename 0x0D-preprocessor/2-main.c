2213f22b6ffdffbe384df298a0441b8d2c9dd156
timmytemmy <timmytemmy241@gmail.com>
2022-11-14 12:29:15 +0100

    Task n

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - using the main function.
+ * Description: prints name of file it was compiles from
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("File :%s\n", __FILE__);
+	return (0);
+}
