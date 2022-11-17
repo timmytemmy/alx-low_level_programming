86201341c3ced747ad30ec6189a7519ce2280df4
timmytemmy <timmytemmy241@gmail.com>
2022-11-17 12:34:14 +0100

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,30 +7,36 @@
  * Return: Always 0.
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *rate;
+	int arg1, arg2, result;
+	char o;
+	int(*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	rate = argv[2];
-	num2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
 
-	if (get_op_func(rate) == NULL || rate[1] != '\0')
+	if (!func)
 	{
 		printf("Error\n");
+		exit(98);
 	}
+	o = *argv[2];
 
-	if ((*rate == '/' && num2 == 0) || (*rate == '%' && num2 == 0))
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(98);
 	}
-	printf("%d\n", get_op_func(rate)(num1, num2));
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
 	return (0);
 }
