ae1948d901cc99ec6dcdccdba052f2c050f1cac3
timmytemmy <timmytemmy241@gmail.com>
2022-11-17 12:46:41 +0100

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	int arg1, arg2, result;
 	char o;
-	int(*func)(int, int);
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
-	
+
 	func = get_op_func(argv[2]);
 
 	if (!func)
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	o = *argv[2];
 
 	if ((o == '/' || o == '%') && arg2 == 0)
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	
+
 	result = func(arg1, arg2);
 
 	printf("%d\n", result);
