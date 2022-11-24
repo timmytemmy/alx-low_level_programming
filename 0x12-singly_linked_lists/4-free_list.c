42e3cc59176ee796660a2774bb18a496db909083
timmytemmy <timmytemmy241@gmail.com>
2022-11-24 21:52:57 +0100

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: head of a linked list.
+ * Return: Always 0.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		list_t *current;
+
+		while ((current = head) != NULL)
+		{
+			head = head->next;
+			free(current->str);
+			free(current);
+		}
+	}
+}
