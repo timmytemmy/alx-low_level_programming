698a3a0d30957c96cfd66695bcdacf9ab33b2611
timmytemmy <timmytemmy241@gmail.com>
2022-12-01 18:35:45 +0100

    Task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -21,5 +21,6 @@ int get_bit(unsigned long int n, unsigned int index)
 			return (n & 1);
 		}
 	}
+	
 	return (-1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,7 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 63)
 		return (-1);
-	
+
 	p = 1 << index;
 
 	if (*n & p)
